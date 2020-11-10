4cdd0fb5f826d4d2e1eb33946c84349d4247b747
Eva Xueyao Guo <56007379+evaxguo@users.noreply.github.com>
2020-11-09 22:21:26 -0500

    Update 3_MC_analysis_2.jl

--- Spatial model (superspreader)/Monte Carlo/3_MC_analysis_2.jl
+++ Spatial model (superspreader)/Monte Carlo/3_MC_analysis_2.jl
@@ -2,31 +2,34 @@ using DataFrames, CSV
 using Statistics
 using Plots
 
-S = CSV.read(joinpath(Pkg.dir("DataFrames"), "/Users/xueyaoguo/Desktop/Monte_Carlo/lamda_1.0/S_df.csv"));
-I = CSV.read(joinpath(Pkg.dir("DataFrames"), "/Users/xueyaoguo/Desktop/Monte_Carlo/lamda_1.0/I_df.csv"));
-R = CSV.read(joinpath(Pkg.dir("DataFrames"), "/Users/xueyaoguo/Desktop/Monte_Carlo/lamda_1.0/R_df.csv"));
-E = CSV.read(joinpath(Pkg.dir("DataFrames"), "/Users/xueyaoguo/Desktop/Monte_Carlo/lamda_1.0/E_df.csv"));
-L = CSV.read(joinpath(Pkg.dir("DataFrames"), "/Users/xueyaoguo/Desktop/Monte_Carlo/lamda_1.0/L_df.csv"));
-ICU = CSV.read(joinpath(Pkg.dir("DataFrames"), "/Users/xueyaoguo/Desktop/Monte_Carlo/lamda_1.0/ICU_df.csv"));
-total_infected = CSV.read(joinpath(Pkg.dir("DataFrames"), "/Users/xueyaoguo/Desktop/Monte_Carlo/lamda_1.0/total_infected.csv"));
+S = CSV.read(joinpath(Pkg.dir("DataFrames"), "./Monte_Carlo/lamda_1.0/S_df.csv")); #TODO: change filepaths
+I = CSV.read(joinpath(Pkg.dir("DataFrames"), "./Monte_Carlo/lamda_1.0/I_df.csv"));
+R = CSV.read(joinpath(Pkg.dir("DataFrames"), "./Monte_Carlo/lamda_1.0/R_df.csv"));
+E = CSV.read(joinpath(Pkg.dir("DataFrames"), "./Monte_Carlo/lamda_1.0/E_df.csv"));
+L = CSV.read(joinpath(Pkg.dir("DataFrames"), "./Monte_Carlo/lamda_1.0/L_df.csv"));
+ICU = CSV.read(joinpath(Pkg.dir("DataFrames"), "./Monte_Carlo/lamda_1.0/ICU_df.csv"));
+total_infected = CSV.read(joinpath(Pkg.dir("DataFrames"), "./Monte_Carlo/lamda_1.0/total_infected.csv"));
 
-nms = [Symbol("t$i") for i in 1:num_MC_simulations]
+num_MC_simulations = 100
+nms = [Symbol("t$i") for i in 1:num_MC_simulations] #an array storing column names for the dataframes
+
+#create a dataframe for the cumulative infection (I+L+ICU+R)
 total_IR = DataFrame()
-for i in 1:100
-    curr = Any[]
-    for j in 1:480
-        I_curr = I[nms[i]][j]
+for i in 1:100 #100 mc simulations
+    curr = Any[] #array to store the cumulative infections for 1 simulation round
+    for j in 1:480 #480 total time steps for each simulation round
+        I_curr = I[nms[i]][j] #get values from the dataframes
         L_curr = L[nms[i]][j]
         ICU_curr = ICU[nms[i]][j]
         R_curr = R[nms[i]][j]
 
-        curr_total_IR = I_curr + L_curr + ICU_curr + R_curr
-        push!(curr, curr_total_IR)
+        curr_total_IR = I_curr + L_curr + ICU_curr + R_curr #cumulative infection at a given point in time = I(t) + L(t) + ICU(t) + R(t)
+        push!(curr, curr_total_IR) 
     end
-    total_IR[nms[i]] = curr
+    total_IR[nms[i]] = curr #add cumulative infection values (time-series) to the dataframe
 end
 
-#CSV.write("/Users/xueyaoguo/Desktop/Monte_Carlo/lamda_1.0/total_IR.csv", total_IR)
+#CSV.write("/Users/xueyaoguo/Desktop/Monte_Carlo/lamda_1.0/total_IR.csv", total_IR) #store as a csv file
 
 #DATAFRAME total_IR
 
